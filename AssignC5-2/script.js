915a465d2984ec1123c6b98304995c275bef59fe
Flavius Mihale <flavius_mihale@yahoo.com>
2022-05-12 01:00:43 +0300

    am oferit raspunsul acolo unde am stiut

--- AssignC5-2/script.js
+++ AssignC5-2/script.js
@@ -3,23 +3,27 @@
 // Sum a and b
 // Return the result
 var myFunction = function (a, b) {
-    return 
+    let sum = a + b;
+    return sum;
+    
 }
 
 
 //2. Write a function that takes two values, say a and b, as arguments
 // Return true if the two values are equal and of the same type
 // Comparison operators, strict equality
-var myFunction2 = function (a, b){
-    return 
+function myFunction2(a, b) {
+    return
 }
-
+// - Flavius update - nu stiu raspunsul - trebuie sa revad video-ul + recomandarea data(link youtube),nu prea am inteles nimic din JS
 
 //3. Write a function that takes a value as argument
 // Return the type of the value
 var myFunction3 = function (a){
-    return 
+    return typeof a;
 }
+
+// - Flavius update - nu stiu raspunsul - trebuie sa revad video-ul + recomandarea data(link youtube),nu prea am inteles nimic din JS
 
 //4. write a function that takes as argument a number,
 // a string, and an object
@@ -28,16 +32,39 @@ var myFunction4 = function (a){
     return 
 }
 
+// - Flavius update - nu stiu raspunsul - trebuie sa revad video-ul + recomandarea data(link youtube),nu prea am inteles nimic din JS
+
 //5. write a Student class that has a constructor with
 // name (will be a String), age (will be a Number), hobbies (will be an Array of Strings) e.g. ["Music", "Travel"]
 // create a method name `greeting` that returns the sentence: Hello my name is {name} and I'm {age}.
 class Student {
 
+constructor(name, age, hobbies) {
+
+    var name = "Flavius";
+    let age = 36;
+    const hobbies = ["Reading", "Biking","Swimming"];
+    
+}     
+    
 }
+
+// - Flavius update - nu stiu raspunsul - trebuie sa revad video-ul + recomandarea data(link youtube),nu prea am inteles nimic din JS
 
 //6. write a Teacher class that has a constructor with
 // name (will be a String), age (will be a Number)
 
+class Teacher {
+
+    constructor(name,age) {
+        
+        var Name = "Flavius";
+        let age = 36;
+
+    }
+}
+
 //7. create two students (instances) of the class defined at 5
 // console log their greetings
 
+// - Flavius update - nu stiu raspunsul - trebuie sa revad video-ul + recomandarea data(link youtube),nu prea am inteles nimic din JS
