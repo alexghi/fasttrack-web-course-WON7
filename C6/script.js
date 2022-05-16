68c6461e7186080f780e3fe3c676d613a914fe81
Alex G <alex.ghiurau@gmail.com>
2022-05-16 21:32:55 +0300

    add C6 course and material

---
+++ C6/script.js
@@ -0,0 +1,12 @@
+const studentNames = ["Alex", "Claudia", "Daniel", "Andrei"];
+
+// console.log(studentNames)
+// console.log(studentNames[studentNames.length - 1])
+
+
+for(var i = 0; i <= studentNames.length - 1; i++) {
+    console.log(i)
+    console.log(studentNames[i])
+
+
+}
