45cfee43f714ac840080585a4282a0c278deebfc
Alex G <alex.ghiurau@gmail.com>
2022-05-16 21:40:56 +0300

    add student setters and getters

--- C6/script.js
+++ C6/script.js
@@ -1,12 +1,46 @@
-const studentNames = ["Alex", "Claudia", "Daniel", "Andrei"];
+class Student {
+    constructor(studentName, age) {
+        this.studentName = studentName
+        this.age = age
+    }
 
-// console.log(studentNames)
-// console.log(studentNames[studentNames.length - 1])
+    greetings() {
+
+    }
+
+    getStudentName() {
+        return this.studentName
+    }
+
+    getAge() {
+        return this.age
+    }
+}
 
 
-for(var i = 0; i <= studentNames.length - 1; i++) {
-    console.log(i)
-    console.log(studentNames[i])
 
 
+const s1 = new Student("Alex")
+
+s1.getStudentName()
+s1.getAge()
+
+
+
+
+
+
+const studentName = ["Sharleen Rollo", "Della Wally", "Ryana Ami", "Lydia Mercy", "Mikey Valorie", "Chester Lexie", "Danette Luanna", "Adalyn Goddard", "Johnnie Peta", "Natille Nigellus"] 
+
+const students = []
+
+for(var i = 0; i < studentName.length; i++ ) {
+
+    students[i] = new Student(studentName[i])
+}
+
+console.log(students)
+
+for(students ) {
+    console.log(student[i].greetings())
 }
