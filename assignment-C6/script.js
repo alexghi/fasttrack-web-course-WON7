df507b6c173224771b10a4b38bf1a859a166319b
Flavius Mihale <flavius_mihale@yahoo.com>
2022-05-22 23:06:20 +0300

    am corectat un rand

--- assignment-C6/script.js
+++ assignment-C6/script.js
@@ -35,8 +35,8 @@ let StudentName = ["Sharleen Rollo", "Della Wally", "Ryana Ami", "Lydia Mercy",
 
 let PhoneNumbers = [12456,78956,4556565,145145151,187984,46345251,216451321,168487494,49846132,965487,256468];
 let age = [25, 30, 31, 33, 25, 22, 29, 34, 35, 36, 24]
-let hobbies = []
-const allStudents = ["snowboarding", "music", "books", "footbal", "programming", "cycling"];
+let hobbies = ["snowboarding", "music", "books", "footbal", "programming", "cycling"]
+const allStudents = [];
 for(let i = 0; i < StudentName.length; i++){
     allStudents[i] = new Student({name : StudentName[i], phone_number: PhoneNumbers[i],
          age: age[i], hobbies: hobbies[i]
--- assignment-C6/student1.js
+++
@@ -1,3 +0,0 @@
-const student1 = student1, {
-    name: , "John Doe": , phone_number: , 0751: , age: , 23: , hobbies: [], "skydiving": 
-};
