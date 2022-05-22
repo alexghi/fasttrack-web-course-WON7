35f39916b9a7c63ce33ce2f1b2f5f0b9cb8d21b0
Dragos <susman_dragos@yahoo.com>
2022-05-22 14:41:51 +0300

    js homework, i didn't know how to do extra-mile 2/2

---
+++ C6/Assignment_C6/script.js
@@ -0,0 +1,43 @@
+class Student{
+    constructor(name, phoneNumber, age, hobbies){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+
+    getHobbies(){
+        return this.hobbies;
+    }
+
+    setHobbies(newHobbies){
+        return this.hobbies = newHobbies;
+    }
+
+    greetings(){
+        return `Hello, my name is ${this.name} and I am ${this.age} of age`
+    }
+}
+
+let student1 = new Student("Angelina Jolie" , "0723111222" , 22, ["Reading" , "Music"]);
+console.log(student1);
+
+console.log(student1.greetings());
+student1.setHobbies(["Tennis" , "Skydiving"])
+console.log(student1.getHobbies());
+
+
+let students = ["Sharleen Rollo" , "Della Wally" , "Ryana Ami" , "Lydia Mercy" , "Mikey Valorey" , "Chester Lexie" , "Danette Luanna", "Adalyn Goddard" , "Johhnie Peta" , "Natille Nigellus"];
+let phone = ["0724582333", "0724582334", "0724582335", "0724582336", "0724582337", "0724582338", "0724582339", "0724582331", "0724582332", "0724582330"]
+let age = [22, 19, 20 ,23, 25, 21, 24, 22, 25, 23]
+let hobbies = [["swimming" , "music"], ["skydiving" , "science"], ["programming", "engineering"] , ["history", "gaming"] , ["arts" , "painting"], ["music", "books"], ["french" , "writing"] , ["music", "books"], ["music" , "books"], ["travelling" , "handball"]]
+let allStudents = []
+
+
+
+
+for(i = 0 ; i<students.length ; i++){
+    allStudents[i] = new Student(students[i], phone[i] , age[i] , hobbies[i]);
+    console.log(allStudents[i]) 
+}
+
