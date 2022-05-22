b920f067a1da7629ebf4e4d876fb791bb210df53
dsfarcas <dani.farcas@gmail.com>
2022-05-22 20:47:20 +0300

    am actualizat tema 06

--- C6/T06-Assignment-C6/script.js
+++ C6/T06-Assignment-C6/script.js
@@ -11,53 +11,82 @@ class student {
     }
 
     getHobbies() {
-        console.log(`${this.name}'hobby is ${this.hobbies}.`)
         return `${this.name}'hobby is ${this.hobbies}.`;
     }
     greetings() {
-        console.log(`“Hello, my name is ${this.name} and I’m ${this.age} of age. My hobby is ${this.hobbies}.`);
         return `Hello, my name is ${this.name} and I’m ${this.age} of age. My hobby is ${this.hobbies}.`;
     }
+
+    getPhoneNo(){
+        return `${this.name}'s phone number is ${this.phoneNumber}.`;
+    }
+    getAge(){
+        return `${this.name} is ${this.age}.`;
+    }
+
 }
 
 const studentNames = ["Sharleen Rollo", "Della Wally", "Ryana Ami", "Lydia Mercy", "Mikey Valorie", "Chester Lexie", "Danette Luanna", "Adalyn", "Goddard", "Johnnie Peta", "Natille Nigellus"];
-const someAge = [25, 19, 25, 25, 21, 20, 27, 25, 22, 29, 29];
+const someAge = [24, 19, 24, 24, 21, 20, 27, 25, 22, 29, 29];
 const somePhoneNo = ["0740 000 111", "0740 111 222", "0740 222 333", "0740 444 555", "0740 555 566", "0740 566 777", "0740 777 888", "0740 888 999", "0740 999 000", "0740 990 011", "0740 000 000"];
 const someHobbies = ['sports', 'music', 'books', 'music', 'astronomy', 'travels', 'piano', 'pets', 'writing', 'books', 'books', 'food'];
 const allStudents = [];
 
 for(i = 0; i < studentNames.length; i++) {
-    // allStudents[i] = new student(studentNames[i]);
     allStudents[i] = new student(studentNames[i]);
-
-        for(i = 0; i < allStudents.length; i++) {
-            allStudents[i] = {
-                name: studentNames[i],
-                age: someAge[i],
-                phoneNumber: somePhoneNo[i],
-                hobbies: someHobbies[i]
-            }
-
-                for(i = 0; i < allStudents.length; i++) {
-                    console.log(allStudents[i].greetings())
-                }
-    }
 }
 console.log(allStudents);
 
-// for(i = 0; i < allStudents.length; i++ ) {
-//     // allStudents[i].greetings();
-//     console.log(allStudents[i].greetings());
+for(i = 0; i < allStudents.length; i++) {
+    allStudents[i] = new student ({
+        name: studentNames[i],
+        age: someAge[i],
+        phoneNumber: somePhoneNo[i],
+        // hobbies: someHobbies[i],
+        // hobbies: "",
+        setHobbies(myHobby) {
+            this.hobbies = myHobby;
+        }
+    })
+    allStudents[i].hobbies = someHobbies[i];    
+    console.log(allStudents[i].greetings()); 
+    console.log(allStudents[i].getHobbies());
+}
+
+for(let i = 0; i < allStudents.length; i++) {
+    if(allStudents[i].hobbies == 'music' || allStudents[i].hobbies == 'books') {
+        console.log(`${allStudents[i].name} is one of the students having ${allStudents[i].hobbies} as a hobby.`)
+    }
+}
+
+for(let i = 0; i < allStudents.length; i++) {
+    if(allStudents[i].age == 24) {
+        console.log(`${allStudents[i].name} is one of the students aged ${allStudents[i].age}.`)
+    }
+}
+
+// TEST
+for(let i = 0; i < allStudents.length; i++) {
+    if (allStudents[i].phoneNumber == "0740 000 111" && allStudents[i].age == 25){
+        console.log(allStudents[i].getPhoneNo());
+        console.log(allStudents[i].getAge());
+    }
+}
+
+
+
+
+
+// for(i = 0; i < allStudents.length; i++){
+//     allStudents[i].setHobbies = someHobbies[i];
 // }
 
 
-// FUNCTIA getHobbies()
-    // S1.getHobbies();
-    // S1.myHobby = "singing";
-    // S1.setHobbies = "singing";
-    // console.log(S1.getGreetings(), S1.getHobbies())
+// const someHobbies = ['sports', 'music', 'books', 'music', 'astronomy', 'travels', 'piano', 'pets', 'writing', 'books', 'books', 'food'];
 
+// allStudents[i].setHobbies()
 
-
-
-
+// for(i = 0; i < allStudents.length; i++) {
+//     allStudents[i].greetings();
+//     console.log(allStudents[i].greetings());
+// }
