6ecd100452f257d7597addf33c32d9cc6def3e23
Alex G <alexg@paymoapp.com>
2022-06-06 21:47:52 +0300

    add c9 resources

---
+++ C9/source/main.js
@@ -0,0 +1,7 @@
+import students, { AAA as showMyFewStudents, addMoreStudents, capitalizedStudents } from "./students";
+
+
+console.log(showMyFewStudents())
+console.log(addMoreStudents(['ZZZ', 'YYY', 'XXX']))
+
+console.log(capitalizedStudents())
