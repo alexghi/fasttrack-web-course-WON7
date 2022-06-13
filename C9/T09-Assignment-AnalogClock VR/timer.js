5247eb9f145cf9a28c5526791dcd83d7014acf41
dsfarcas <dani.farcas@gmail.com>
2022-06-13 10:17:32 +0300

    Clock / Timer - work in progress (still working on buttons)

--- C9/T09-Assignment-AnalogClock VR/timer.js
+++ C9/T09-Assignment-AnalogClock VR/timer.js
@@ -1,37 +1,8 @@
-//========================= CLOCK
+
+
 let secondHand = document.querySelector(".second");
 let minHand = document.querySelector(".minute");
 let hourHand = document.querySelector(".hour");
-
-const myClockInterval = setInterval(clockRotating, 1000);
-
-function clockRotating() {
-  var date = new Date();
-  var getSeconds = date.getSeconds() / 60;
-  var getMinutes = date.getMinutes() / 60;
-  var getHours = date.getHours() / 12;
-
-  secondHand.style.transform = "rotate(" + getSeconds * 360 + "deg)";
-  minHand.style.transform = "rotate(" + getMinutes * 360 + "deg)";
-  hourHand.style.transform = "rotate(" + getHours * 360 + "deg)";
-
-  const days = ["duminică", "luni", "marți", "miercuri", "joi", "vineri", "sâmbătă"];
-  const months = ["ianuarie", "februarie", "martie", "aprilie", "mai", "iunie", "iulie", "august", "septembrie", "octombrie", "noiembrie", "decembrie"];
-  var Day = days[date.getDay()]
-  var Month = months[date.getMonth()];
-  var DayInMonth = date.getDate();
-  var Year = date.getFullYear();
-  
-  document.querySelector(".current-day-in-week").innerHTML = `${Day}`;  
-  document.querySelector(".current-day").innerHTML = `${DayInMonth} ${Month} ${Year}`;
-//   document.querySelector(".current-day").innerHTML = date.toDateString();
-}
-
-function clockStop() {
-  clearInterval(myClockInterval);
-}
-
-//========================= TIMER
 
 var seconds = 0;
 
@@ -58,7 +29,6 @@ const myTimerInterval = setInterval(timerOn, 1000);
 
 function timerStop() {
     clearInterval(myTimerInterval);
-    clockRotating();
 }
 
 function timerReset() {
@@ -68,5 +38,4 @@ function timerReset() {
     hourHand.style.transform = "rotate(" + 0 * 360 + "deg)";
     document.querySelector("#timerOutput").innerHTML = `0:0:0`
 }
-
 
