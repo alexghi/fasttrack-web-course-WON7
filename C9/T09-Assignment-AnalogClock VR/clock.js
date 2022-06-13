5022cccc0cfb8ef797454e63db49efd83efcaed0
dsfarcas <dani.farcas@gmail.com>
2022-06-13 14:44:01 +0300

    timer code updated

--- C9/T09-Assignment-AnalogClock VR/clock.js
+++ C9/T09-Assignment-AnalogClock VR/clock.js
@@ -36,21 +36,23 @@ function clockStop() {
 var seconds = 0;
 
 function timerOn() {
-    seconds++;
-    console.log(seconds)
-   
-    const mySeconds = seconds / 60;
-    const myMinutes = Math.floor(seconds / 3600);
-    const myHours = Math.floor(seconds / 3600);
-    console.log('minutele ', myMinutes)
-    secondHand.style.transform = "rotate(" + mySeconds * 360 + "deg)";
-    minHand.style.transform = "rotate(" + myMinutes * 360 + "deg)";
-    hourHand.style.transform = "rotate(" + myHours * 360 + "deg)";
-    console.log('aici', mySeconds, myMinutes)
-    console.log('tot aici', mySeconds, myMinutes)
-    
-    document.querySelector("#timerOutput").innerHTML = `${myHours}:${myMinutes}:${Math.floor(seconds)}`;   
+  seconds++;
+  console.log(seconds);
+  
+  const mySeconds = seconds / 60;
+  const secondsToSubstract = (Math.floor(seconds / 60) * 60);
+  const myMinutes = Math.floor(seconds / 60);
+  const minutesToSubstract = (Math.floor(seconds / 3600) * 60);
+  const myHours = Math.floor(seconds / 3600);
+  
+  secondHand.style.transform = "rotate(" + mySeconds * 360 + "deg)";
+  minHand.style.transform = "rotate(" + myMinutes * 0.016 * 360 + "deg)";
+  hourHand.style.transform = "rotate(" + myHours * 0.016 * 360 + "deg)";
+  console.log('aici', mySeconds, myMinutes)
+  
+  document.querySelector("#timerOutput").innerHTML = `${myHours}:${myMinutes - minutesToSubstract}:${Math.floor(seconds) - secondsToSubstract}`;
 }
+
 const myTimerInterval = setInterval(timerOn, 1000);
 
 function timerStop() {
@@ -65,5 +67,23 @@ function timerReset() {
     hourHand.style.transform = "rotate(" + 0 * 360 + "deg)";
     document.querySelector("#timerOutput").innerHTML = `0:0:0`
 }
+
+
+// function timerOn() {
+//   seconds++;
+//   console.log(seconds)
+ 
+//   const mySeconds = seconds / 60;
+//   const myMinutes = Math.floor(seconds / 3600);
+//   const myHours = Math.floor(seconds / 3600);
+//   console.log('minutele ', myMinutes)
+//   secondHand.style.transform = "rotate(" + mySeconds * 360 + "deg)";
+//   minHand.style.transform = "rotate(" + myMinutes * 360 + "deg)";
+//   hourHand.style.transform = "rotate(" + myHours * 360 + "deg)";
+//   console.log('aici', mySeconds, myMinutes)
+//   console.log('tot aici', mySeconds, myMinutes)
+  
+//   document.querySelector("#timerOutput").innerHTML = `${myHours}:${myMinutes}:${Math.floor(seconds)}`;   
+// }
 
 
