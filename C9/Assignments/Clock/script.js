eccba1c5601fdc8deb7925dbacf358a7f110345e
Bettina <sanyobettina@gmail.com>
2022-06-18 20:13:14 +0300

    clock/game

--- C9/Assignments/Clock/script.js
+++ C9/Assignments/Clock/script.js
@@ -13,7 +13,7 @@ primeste ca prim parametru o functie care se va autoinvoca (se va apela) dupa un
 in al doilea parametru
 setInterval, se apeleaza la fiecare ms
 */
-
+/*
 setTimeout(function() {
     console.log('aici dupa o secunda')
 }, 1000)
@@ -23,6 +23,32 @@ setInterval(function() {
 }, 1000) // valoare exprimata in ms 1000ms = 1s 
 
 console.log(`acum - `, new Date())
-
+*/
 // functiile din acest fisier se pot inlocui cu cele pe care le veti face voi
 // sunt doar ca sa aveti cateva lucruri de la care sa porniti
+
+const minuteEl = document.querySelector(".minute--hand");
+const secondEl = document.querySelector(".second--hand");
+const hourEl = document.querySelector(".hour--hand");
+
+function setDate(){
+    const now = new Date;
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds/60)*360)+90;
+    secondEl.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const mindDegrees = ((mins/60)*360)+((seconds/60)*6)+90;
+    minuteEl.style.transform = `rotate(${mindDegrees})`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour /12)*360)+((mins/60)*30)+90;
+    hourEl.style.transform = `rotate(${hourDegrees}deg)`;
+
+} 
+setInterval(setDate,1000);
+setDate();
+
+
+
+
