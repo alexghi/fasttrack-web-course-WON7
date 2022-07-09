9c68b910c5f6cdaa32d345f38c257098c9bdf144
dsfarcas <dani.farcas@gmail.com>
2022-07-09 09:09:56 +0300

    Weather assignment. Nu am realizat functia de toggle

--- C12/T12-Assignment-02 [WEATHER]/weather.js
+++ C12/T12-Assignment-02 [WEATHER]/weather.js
@@ -1,172 +1,217 @@
 async function loadWeather() {
-    console.log('weather');
+  console.log("weather");
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      
-    const data = await fetch("https://weatherdbi.herokuapp.com/data/weather/Oradea", requestOptions)
-        .then(response => response.json())
-        // .then(result => console.log(result))
-        .then(result => result)
-        .catch(error => console.log('error', error));
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
-     console.log('mydata',  data.next_days);
-     const filteredData = data.next_days;
-          console.log('filtered data 1', filteredData);
-        
-    
-    for(var i = 0; i < filteredData.length; i++) {
-        var dayDiv = document.createElement('div');
-        var someDay = filteredData[i];
-        dayDiv.time = filteredData[i].day;
-             console.log(dayDiv.time);
-        // dayDiv.maxCelsius = filteredData[i].max_temp.c;
-        //     console.log(dayDiv.maxCelsius)
-        // dayDiv.icon = filteredData[i].iconURL;
-        //     console.log(dayDiv.icon);
-        // dayDiv.setAttribute('id', 'w-days');
-        // document.getElementById('w-body').appendChild(dayDiv);
-        // dayDiv.innerHTML = `${dayDiv.time} \n ${dayDiv.maxCelsius} \n ${dayDiv.icon}`
+  const data = await fetch(
+    "https://weatherdbi.herokuapp.com/data/weather/Oradea",
+    requestOptions
+  )
+    .then((response) => response.json())
+    // .then(result => console.log(result))
+    .then((result) => result)
+    .catch((error) => console.log("error", error));
 
-        var myDay = dayDiv.time;
-        var maxCelsius = dayDiv.maxCelsius = someDay.max_temp.c;
-             console.log(dayDiv.maxCelsius);
-        var maxFahrenheit = dayDiv.maxCelsius = someDay.max_temp.f;
-             console.log(dayDiv.maxFahrenheit);
-        var minCelsius = dayDiv.minCelsius = someDay.min_temp.c;
-             console.log(dayDiv.minCelsius);
-        var minFahrenheit = dayDiv.minFahrenheit = someDay.min_temp.f;
-             console.log(dayDiv.minFahrenheit);
-        
-             
-        // dayDiv.className = 'w-day';
-        dayDiv.setAttribute('id', `w-day-${i}`);
-        dayDiv.innerHTML = `${myDay}  \n ${maxCelsius}ºC`; // \n ${minCelsius}ºC \n ${maxFahrenheit}ºF \n ${minFahrenheit}ºF
-        document.getElementById('w-body').appendChild(dayDiv);
-             console.log(dayDiv);
+  window.weatherData = data;
+  // =================  HEADER
+        const location = data.region;
+        console.log(location);
+        document.getElementById(
+            "w-header-l-b"
+        ).innerHTML = `${data.region}, 47° 04' 0.01" N 21° 55' 59.99" E`;
+        const currentTemp = data.currentConditions.temp.c;
+        document.getElementById("w-header-r-t").innerHTML = `${currentTemp} ºC`;
+        const currentTime = data.currentConditions.dayhour;
+        document.getElementById("w-header-r-b").innerHTML = `${currentTime}`;
 
- 
-             // IMAGES
-        var img = document.createElement('img');
-        img.src = someDay.iconURL;
-             console.log(img.src)
-        img.alt = 'weather pic';
-        // img.className = 'w-icon';
-        img.setAttribute('id', 'w-icon')
-        document.getElementById(`w-day-${i}`).appendChild(img);
-    
+  // ================= BODY
+        for (var i = 0; i < data.next_days.length; i++) {
+            var dayData = data.next_days[i];
+            console.log(dayData);
+            var currentDayName = data.next_days[i].day;
+            console.log(currentDayName);
+            var dayName = data.next_days[i].day;
+            console.log(dayName);
+            var imageURL = data.next_days[i].iconURL;
+            console.log(imageURL);
+            var minTempC = data.next_days[i].min_temp.c;
+            console.log(minTempC);
+            var minTempF = data.next_days[i].min_temp.f;
+            console.log(minTempF);
+            var maxTempC = data.next_days[i].max_temp.c;
+            console.log(maxTempC);
+            var maxTempF = data.next_days[i].max_temp.f;
+            console.log(maxTempF);
 
+            // Prepare the proper display of the day after tomorrow and on
+            const d = new Date();
+            const currentMonth = d.getMonth() + 1;
+            const currentDayInMonth = d.getDate();
+            const currentMonthDay = `${currentMonth} - ${currentDayInMonth}`;
+            var increment = data.next_days.indexOf(data.next_days[i]);
+            const incrementedDays = currentDayInMonth + increment;
 
+            // PARENT DIV: creating w-day-${i}. This div will include all the information for every particular day
+            var dayDiv = document.createElement("div");
+            dayDiv.setAttribute("id", `w-day-${i}`);
+            dayDiv.setAttribute(
+            "style",
+            "display:flex; flex-direction: column; padding:5px;margin:1px; width: 150px;"
+            );
+            // dayDiv.innerHTML = `${dayName}`;
+            // if(dayName == data.next_days[0].day) {
+            //      dayDiv.innerHTML = `${dayName} \n Today`;
+            // } else if(dayName == data.next_days[1].day) {
+            //      dayDiv.innerHTML = `${dayName} \n Tomorrow`;
+            // }
+            // else {
+            //      dayDiv.innerHTML = `${dayName} \n ${currentMonth} - ${incrementedDays}`;
+            // }
+            document.getElementById("w-body").appendChild(dayDiv);
 
-        //// === INCERCARE ALTERNATIVA     #1  
-        //     // DAY - am filtrat datele ca sa trec direct la datele pentru next_days (care includ si ziua curenta)
-        // var someDay = filteredData[i];
+            // DAY
+            var dayNameDiv = document.createElement("div");
+            dayNameDiv.setAttribute("id", `w-name-${i}`);
+            dayNameDiv.setAttribute(
+            "style",
+            "font-weight:bolder; font-variant: small-caps; padding-top:5px;"
+            );
+            dayNameDiv.innerHTML = `${dayName}`;
+            document.getElementById(`w-day-${i}`).appendChild(dayNameDiv);
 
-        //     // DAY ARRAY (0 TO 7 DAYS)
-        // var dayArr = [];
-        // for(var i = 0; i < filteredData.length; i++) {
-        //      dayArr.push(filteredData[i].day);
-        //  }
-        //     console.log(dayArr)
-        
-        //     // PARCURG dayArr
-        // for(var i = 0; i <= dayArr.length-1; i++) {
-        //     var dayDiv = document.createElement('div');
-        //     var myDay = dayArr[i];
-        //         console.log('dayDiv', myDay, '//', dayArr[i]);
-             
-        //     dayDiv.setAttribute('id', 'w-day');
-        //     // dayDiv.innerHTML = `${myDay}  `; // \n ${maxCelsius}ºC \n ${minCelsius}ºC \n ${maxFahrenheit}ºF \n ${minFahrenheit}ºF
-        //     document.getElementById('w-body').appendChild(dayDiv);
-        //         console.log(dayDiv);
+            // DATE
+            var dateDiv = document.createElement("div");
+            dateDiv.setAttribute("id", `w-date-${i}`);
+            dateDiv.setAttribute("style", "padding-top:5px;");
+            if (dayName == data.next_days[0].day) {
+            dateDiv.innerHTML = `Today`;
+            } else if (dayName == data.next_days[1].day) {
+            dateDiv.innerHTML = `Tomorrow`;
+            } else {
+            dateDiv.innerHTML = `${currentMonth} - ${incrementedDays}`;
+            }
+            document.getElementById(`w-day-${i}`).appendChild(dateDiv);
 
-        // }
+            // IMAGES
+            var img = document.createElement("img");
+            img.src = imageURL;
+            console.log(img.src);
+            img.alt = "missing pic";
+            img.setAttribute("id", `w-icon-${i}`);
+            document.getElementById(`w-day-${i}`).appendChild(img);
 
-        //     // CREEZ <img/> IN <dayDiv/>, PENTRU FIECARE ZI, CU URL-UL AFERENT
-        // var imageArr = [];
-        // for(i = 0; i < filteredData.length; i++) {
-        //     imageArr.push(filteredData[i].iconURL);
-        // }
-        //     console.log(imageArr)
-        //     // PARCURG imageArr
-        // for(var i = 0; i <= imageArr.length; i++) {
-        //     // var image = someDay[i];
-        //     // var image = filteredData[i].imageURL;
-        //     var img = document.createElement('img');
-        //     var myImg = imageArr[i];
-        //         console.log('myImg', myImg, '//', imageArr[i])
-        //     img.src = myImg;
-        //         console.log(img.src)
-        //     img.alt = 'pic missing';
-        //     img.setAttribute('id', 'w-icon');
-        //     document.getElementById('w-day').appendChild(img);
-        // }
-        
+            // MAX TEMPERATURE
+            var maxTempDiv = document.createElement("div");
+            maxTempDiv.setAttribute("id", `w-maxTemp-${i}`);
+            maxTempDiv.innerHTML = `${maxTempC}ºC`;
+            if (maxTempC >= 30) {
+            maxTempDiv.setAttribute("style", "background-color: #ffff00");
+            } else if (maxTempC >= 25 && maxTempC <= 29) {
+            maxTempDiv.setAttribute("style", "background-color: #ffffb3");
+            } else if (maxTempC >= 20 && maxTempC <= 24) {
+            maxTempDiv.setAttribute("style", "background-color: #ffffe6");
+            } else if ((maxTempC >= 15) & (maxTempC <= 19)) {
+            maxTempDiv.setAttribute("style", "background-color: #00cc41");
+            } else if ((maxTempC >= 10) & (maxTempC <= 14)) {
+            maxTempDiv.setAttribute("style", "background-color: #80ffa8");
+            } else if ((maxTempC >= 5) & (maxTempC <= 9)) {
+            maxTempDiv.setAttribute("style", "background-color: #e6ffee");
+            } else if ((maxTempC >= 1) & (maxTempC <= 4)) {
+            maxTempDiv.setAttribute("style", "background-color: #99ffdd");
+            } else if ((maxTempC = 0 & (maxTempC >= -5))) {
+            maxTempDiv.setAttribute("style", "background-color: #e6ffff");
+            } else if ((maxTempC <= -6) & (maxTempC >= -10)) {
+            maxTempDiv.setAttribute("style", "background-color: #66ffff ");
+            } else {
+            maxTempDiv.setAttribute("style", "background-color: #00cccc");
+            }
+            document.getElementById(`w-day-${i}`).appendChild(maxTempDiv);
 
-        ////===ALTERNATIVA    #2
-        // var dayDiv = document.createElement('div');
-        // var someDay = filteredData[i];
-        // dayDiv.time = filteredData[i].day;
-        //      console.log(dayDiv.time);
-        // dayDiv.maxCelsius = filteredData[i].max_temp.c;
-        //     console.log(dayDiv.maxCelsius)
-        // dayDiv.icon = filteredData[i].iconURL;
-        //     console.log(dayDiv.icon);
-        // dayDiv.setAttribute('id', 'w-days');
-        // document.getElementById('w-body').appendChild(dayDiv);
-        // dayDiv.innerHTML = `${dayDiv.time} \n ${dayDiv.maxCelsius} \n ${dayDiv.icon}`
-
-    }
-
-
-
-
-    // for(i = 0; i < filteredData.length; i++) {
-    //     var image = someDay[i];
-    //     var img = document.createElement('img');
-    //     var myImg = someDay.iconURL;
-    //     img.src = someDay.iconURL;
-    //         console.log(img.src)
-    //     img.alt = 'weather pic';
-    //     img.className = 'w-icon';
-    //     document.querySelector('.w-day').appendChild(img);
-    //         console.log(img.src)
-    // }
-
-    // for(i = 0;  i < filteredData.length; i++) {
-    //     var maxCelsius = filteredData[i].max_temp[i];
-    //         console.log('maxima in grade Celsius', maxCelsius);
-    //     var maxCelsiusDiv = document.createElement('div');
-    //     maxCelsiusDiv.className = 'w-maxCelsius';
-    //     maxCelsiusDiv.innerHTML = `${maxCelsius}`;
-    //     document.getElementsByClassName('w-day').appendChild(maxCelsiusDiv);
-    // }
-
-
-
-    const location = data.region;
-            console.log(location);
-    document.getElementById('w-header-l-b').innerHTML = `${data.region}, 47° 04' 0.01" N 21° 55' 59.99" E`;    
-    const currentTemp = data.currentConditions.temp.c;
-    document.getElementById('w-header-r-t').innerHTML = `${currentTemp} ºC`;
-    const currentTime = data.currentConditions.dayhour;
-    document.getElementById('w-header-r-b').innerHTML = `${currentTime}`;
-
-        // const currentTime = new Date();
-        // console.log(currentTime);
-        // const currentHour = currentTime.getHours();
-        // const currentMinutes = currentTime.getMinutes();
-
-
+            // MIN TEMPERATURE
+            var minTempDiv = document.createElement("div");
+            minTempDiv.setAttribute("id", `w-minTemp-${i}`);
+            minTempDiv.innerHTML = `${minTempC}ºC`;
+            if (minTempC >= 30) {
+            minTempC.setAttribute("style", "background-color: #ffff00");
+            document.documentElement.style.setProperty(
+                "--temperature-color1",
+                "pink"
+            );
+            } else if (minTempC >= 25 && minTempC <= 29) {
+            minTempDiv.setAttribute("style", "background-color: #ffffb3");
+            } else if (minTempC >= 20 && minTempC <= 24) {
+            maxTempDiv.setAttribute("style", "background-color: #ffffe6");
+            } else if ((minTempC >= 15) & (minTempC <= 19)) {
+            minTempDiv.setAttribute("style", "background-color: #00cc41");
+            } else if ((minTempC >= 10) & (minTempC <= 14)) {
+            minTempDiv.setAttribute("style", "background-color: #80ffa8");
+            } else if ((minTempC >= 5) & (minTempC <= 9)) {
+            minTempDiv.setAttribute("style", "background-color: #e6ffee");
+            } else if ((minTempC >= 1) & (minTempC <= 4)) {
+            minTempDiv.setAttribute("style", "background-color: #99ffdd");
+            } else if ((minTempC = 0 & (minTempC >= -5))) {
+            minTempDiv.setAttribute("style", "background-color: #e6ffff");
+            } else if ((minTempC <= -6) & (minTempC >= -10)) {
+            minTempDiv.setAttribute("style", "background-color: #66ffff ");
+            } else {
+            minTempDiv.setAttribute("style", "background-color: #00cccc");
+            }
+            document.getElementById(`w-day-${i}`).appendChild(minTempDiv);
+        }
 }
 loadWeather();
 
+//===
+console.log(weatherData)
+var C = weatherData.next_days[i].max_temp.c;
+  console.log(C);
+  console.log(weatherData.next_days[1].max_temp.c);
+var F = weatherData.next_days[i].max_temp.f;
+  console.log(F);
+  console.log(weatherData.next_days[1].max_temp.c);
+// var unit = C;
+
+toggleMeasurementUnit = () => {
+  measurementUnit === C ? (measurementUnit = F) : (measurementUnit = C);
+  generateForecast();
+  return measurementUnit;
+};
+
+generateForecast = () => {
+  for(i = 0; i < weatherData.next_days.length; i++) {
+    console.log(weatherData.next_days[i].max_temp.c)
+  }
+};
+
+var measure = weatherData.next_days[i].max_temp[0];
+measure.forEach(function (item) {
+    console.log(item)
+})
+console.log(measure.forEach)
 
 
-// var myImage = filteredData[0].iconURL;
-// var imago = document.createElement('img');
-// console.log(myImage)
-// document.getElementById('temp').appendChild(imago);
-// document.getElementsByClassName('temp').innerHTML = myImage;
+//=== A
+// var measurementUnit = "C";
+// toggleMeasurementUnit = () => {
+//   measurementUnit === "C" ? (measurementUnit = "F") : (measurementUnit = "C");
+//   generateForecast();
+//   return measurementUnit;
+//   // if(measurementUnit === 'C') {
+//   //     measurementUnit = 'F'
+//   // }
+// };
+
+// generateForecast = () => {
+//   weatherData.forEach(function (item) {
+//     console.log(item);
+//   });
+// };
+
+// var measure = weatherData.next_days[i].max_temp[0];
+// measure.forEach(function (item) {
+//     console.log(item)
+// })
+// console.log(measure.forEach)
